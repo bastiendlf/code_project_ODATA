8639396fba9ff2083ab27fca876b62bd9924b243
Bastien DELFORGE <bastien.delforge@etu.imt-lille-douai.fr>
2020-11-04 21:37:47 +0100

    improving gallery_probes_generator.py with safety and merged probes eigenface_notebook to try ACP on gallery

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from gallery_probes_generator import GalleryProbesGenerator
 
+import numpy as np
+
 
 data_generator = GalleryProbesGenerator(path_to_dataset='data/dataset1/', probes_length=200)
 
-#data_generator.generate_npy_files()
-
+data_generator.generate_npy_files()
 
 probe_registered_names, probe_registered_pictures = data_generator.get_registered_probes()
 probe_unregistered_names, probe_unregistered_pictures = data_generator.get_unregistered_probes()
 gallery_names, gallery_pictures = data_generator.get_gallery()
+probe_merged_names, probe_merged_pictures = data_generator.get_merged_probes()
+
+print("hello")
+
+# val_propres = np.load('data/dataset1/eigenface_npy/val_propres.npy', allow_pickle=True)
+# vect_propres = np.load('data/dataset1/eigenface_npy/vect_propres.npy', allow_pickle=True)
+# w = np.load('data/dataset1/eigenface_npy/w.npy', allow_pickle=True)
